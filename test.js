a1e01be661afa82c6748665a4d6626a2110930c8
Andrew640 <andrew.thomas640@gmail.com>
2014-05-15 13:00:55 +0100

    Node for twitter testing

---
+++ test.js
@@ -0,0 +1,20 @@
+function iPonda() {
+
+    console.log('This has been called');
+
+}
+
+iPonda.prototype.setGetField = function(arg1, arg2) {
+
+    console.log('Whahahayyyy', arg1, arg2);
+
+};
+
+module.exports = iPonda;
+
+
+
+// module.exports = {
+//     objectkey1: 'asd',
+//     objectkey2: 'qwd'
+// };
